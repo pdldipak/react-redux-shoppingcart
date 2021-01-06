3acfbdace050463e445218acadd37088b0027785
Dipak Poudel <pdl.dipak@gmail.com>
2021-01-06 10:17:28 +0100

    setup project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+      <a href="/" >React Shopping card</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right are preserved</footer>
+    </div>
+  );
+}
+
+export default App;
